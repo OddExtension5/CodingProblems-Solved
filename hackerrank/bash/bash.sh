37e1a87ffcc40efec4ae02b390e4cdf4ad89ee43
Sushil Singh <36241112+OddExtension5@users.noreply.github.com>
2020-07-31 10:14:45 +0530

    Update bash.sh

--- hackerrank/bash/bash.sh
+++ hackerrank/bash/bash.sh
@@ -73,6 +73,89 @@ else
     echo "X is less than Y"
 fi
 
+##### PROBLEM 6: Getting started with conditionals #####
+:'
+Read in one character from STDIN.
+If the character is 'Y' or 'y' display "YES".
+If the character is 'N' or 'n' display "NO".
+No other character will be provided as input.
+'
+read m
+
+if [[ $m == Y || $m == y ]];
+then
+    echo "YES"
+fi
+
+if [[ $m == N || $m == n ]];
+then 
+    echo "NO"
+fi
+
+##### PROBLEM 7: More on Conditionals #####
+:'
+Given three integers (X, Y, and Z ) representing the three sides of a triangle, identify whether the triangle is scalene, 
+isosceles, or equilateral.
+
+If all three sides are equal, output EQUILATERAL.
+Otherwise, if any two sides are equal, output ISOSCELES.
+Otherwise, output SCALENE.
+'
+
+read X
+read Y
+read Z
+
+if [[ $X == $Y && $Y == $Z ]];
+then
+    echo "EQUILATERAL"
+elif [[ $X != $Y && $Y != $Z && $Z != $X ]];
+then
+    echo "SCALENE"
+else
+    echo "ISOSCELES"
+fi
 
 
+##### PROBLEM 8: Arithmetic Operations #####
+:'
+A mathematical expression containing +,-,*,^, / and parenthesis will be provided. 
+Read in the expression, then evaluate it. Display the result rounded to 3 decimal places.
+'
+
+read exp
+value=` echo "$exp" | bc -l`
+printf "%.3f" $value
+
+
+##### PROBLEM 9: Compute the Average #####
+:'
+Given N integers, compute their average, rounded to three decimal places.
+'
+
+read n
+arr=($(cat))
+arr=${arr[*]}
+printf "%.3f" $(echo $((${arr// /+}))/$n | bc -l)
+
+# OR
+
+read n
+printf "%.3f" $(echo "("$(cat)")/$n" | tr ' ' '+' | bc -l)
+
+# OR
+
+read n
+sum=0
+for (( i=0; i<$n; i++ ));
+do
+    read temp
+    sum=$(($sum+$temp))
+done
+printf "%.3f\n" $(bc -l <<< "$sum/$n")
+
+#### PROBLEM 10: Functions and Fractals - Recursive Trees #####
+:'
+Problem Link: https://www.hackerrank.com/challenges/fractal-trees-all/problem?h_r=next-challenge&h_v=zen
+'
 
